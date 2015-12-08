143ea07ffc8d68ee6d49a1d2bdc0ec5155700c91
Sean Esteva <seangeleno@gmail.com>
2015-12-07 16:23:43 -0800

    added mainCtrl.js

--- public/app/app.js
+++ public/app/app.js
@@ -1,9 +1,9 @@
 angular.module('userApp', ['ngAnimate', 'app.routes', 'mainCtrl', 'userCtrl', 'userService', 'authService'])
 
 // application configuration to integrate token into requests
-.config(function() {
+.config(function($httpProvider) {
 
 	// attach our auth interceptor to the http requests
-
+	$httpProvider.interceptors.push('AuthInterceptor')
 
 });
--- public/app/controllers/mainCtrl.js
+++ public/app/controllers/mainCtrl.js
@@ -1,27 +1,55 @@
 angular.module('mainCtrl', [])
 
-.controller('mainController', function() {
+.controller('mainController', function($rootScope, $location, Auth) {
 
-	var vm = this;
+	var mainCtrl = this;
 
 	// get info if a person is logged in
 
+	mainCtrl.loggedIn = Auth.isLoggedIn()
 
 	// check to see if a user is logged in on every request
 
+	$rootScope.$on('$routeChangeStart', function(){
+		mainCtrl.loggedIn = Auth.isLoggedIn();
+
 
 		// get user information on page load
-
-
+		Auth.getUser()
+			.then(functino(data){
+				mainCtrl.user = data.data
+			})
+	})
 	// function to handle login form
 
+	mainCtrl.doLogin = function(){
+		mainCtrl.processing = true
+	}
 
 		// clear the error
 
+		mainCtrl.error = ''
+
 				// if a user successfully logs in, redirect to users page
+				Auth.login(mainCtrl.loginData.username, mainCtrl.loginData.password)
+					.success(function(){
+							mainCtrl.processing = false
 
-
+							if ( data.success ){
+								$location.path('/users')
+							}
+							else {
+								mainCtrl.error = data.message
+							}
+					})
 	// function to handle logging out
+	mainCtrl.doLogout = function(){
+		Auth.logout()
+		//empty out user
+		mainCtrl.user = ''
 
+		//redirect to login screen
+		$location.path('/login')
+	}
 
 });
